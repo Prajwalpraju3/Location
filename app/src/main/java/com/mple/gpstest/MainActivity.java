5da0ae2c803f155eb51c0e19d8dbe8b4cc6c0317
Prajwal <prajwal@22feettribalww.com>
2019-07-04 11:22:49 +0530

    permission added

--- app/src/main/java/com/mple/gpstest/MainActivity.java
+++ app/src/main/java/com/mple/gpstest/MainActivity.java
@@ -3,6 +3,7 @@ package com.mple.gpstest;
 import androidx.annotation.NonNull;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import android.Manifest;
 import android.content.IntentSender;
@@ -111,23 +112,39 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void startLocationUpdates() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-//            Log.d("ttt", "startLocationUpdates: ---------------->");
-            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    Activity#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for Activity#requestPermissions for more details.
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            return;
+        }else{
+            // Write you code here if permission already given.
+            fusedLocationClient.requestLocationUpdates(createLocationRequest(),
+                    locationCallback,
+                    null );
+            Log.d("ttt", "startLocationUpdates: ---------------->");
+        }
+
+
+    }
+
+
+
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case 1: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startLocationUpdates();
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
                 return;
             }
+            // other 'case' lines to check for other
+            // permissions this app might request
         }
-        Log.d("ttt", "startLocationUpdates: ---------------->");
-        fusedLocationClient.requestLocationUpdates(createLocationRequest(),
-                locationCallback,
-                null /* Looper */);
     }
 
     @Override
